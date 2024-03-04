9ddae8240096d322d81e58cf197ba25484619d4f
Akash Shendkar <155044678+AkashShendkar@users.noreply.github.com>
2024-03-04 11:19:44 +0530

    Update script.sh

--- wordpress-website-with-ec2/script.sh
+++ wordpress-website-with-ec2/script.sh
@@ -1,31 +1,17 @@
 #!/bin/bash
-
-yum update
-
-wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
-
-rpm -ivh mysql-community-release-el7-5.noarch.rpm
-
-sudo yum install mysql-server httpd php php-mysqlnd -y
-
-
-
-cd /var/www/html/
-# Create html file
-echo "healthy" > healthy.html
+sudo yum update
+sudo yum install mariadb105-server.x86_64 httpd php php-mysqlnd -y
 
 wget https://wordpress.org/latest.tar.gz
 
 tar -xzf latest.tar.gz
 
-mv -r wordpress/* /var/www/html/
-
-
+sudo mv wordpress/* /var/www/html/
 
 sudo chmod -R 755 wp-content
 sudo chown apache:apache /var/www/html
-sudo chkconfig httpd on  
-sudo service httpd start
+sudo systemctl start httpd
+sudo systemctl enable httpd
 
 
 
